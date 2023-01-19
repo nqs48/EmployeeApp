4aaebccd9baad1710155dff9aef6c8c064b3ae5d
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-01-19 14:53:19 -0500

    Adding model Employee with Properties

---
+++ EmployeeApp.Domain/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmployeeCode { get; set; }
+        public string UrlPhoto { get; set; }
+        public string Email { get; set; }
+        public int Age {get; set;}
+        public DateTime HireDate { get; set;}
+        public DateTime? TerminationDate { get; set;}
+
+
+    }
+}
