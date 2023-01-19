bf489d0b1fa9d54536b787e335373eb86007ad7c
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-01-19 18:40:30 -0500

    Adding DTO for Employee

---
+++ EmployeeApp.Domain/DTO/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Domain.DTO
+{
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public string EmployeeCode { get; set; }
+        public string UrlPhoto { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+    }
+}
