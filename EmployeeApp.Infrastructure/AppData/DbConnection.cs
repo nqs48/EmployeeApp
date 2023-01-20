e24e9596df10415d390d6af0125a93dc177ef630
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-01-20 12:01:42 -0500

    Adding db connection class

---
+++ EmployeeApp.Infrastructure/AppData/DbConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Infrastructure.AppData
+{
+    public class DbConnection
+    {
+        private string connectionString;
+
+        public string ConnectionString { get => connectionString; }
+
+        public DbConnection(string Connection)
+        {
+            connectionString= Connection;
+        }
+    }
+}
