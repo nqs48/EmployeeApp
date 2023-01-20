8ab522db69b4ab6c3324b74a4a194ee866ecfc39
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-01-20 12:00:51 -0500

    Configurin conection sql with singleton

--- EmployeeApp.Api/Program.cs
+++ EmployeeApp.Api/Program.cs
@@ -1,9 +1,16 @@
 //Usings
+
+using EmployeeApp.Infrastructure.AppData;
 using EmployeeApp.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var config = builder.Configuration;
+var stringConnection = new DbConnection(config.GetConnectionString("SQL"));
+builder.Services.AddSingleton(stringConnection);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
