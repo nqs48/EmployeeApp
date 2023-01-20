987e30b179975d0471c8a7240e5f55104bf3c278
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-01-19 21:55:25 -0500

    Adding Validators with DataNotations

--- EmployeeApp.Domain/DTO/EmployeeDTO.cs
+++ EmployeeApp.Domain/DTO/EmployeeDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,22 @@ namespace EmployeeApp.Domain.DTO
 {
     public class EmployeeDTO
     {
+        [Required(ErrorMessage = "Field Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field Employee Code is required")]
+        [MaxLength(4,ErrorMessage = "Max length must be 4 characters")]
         public string EmployeeCode { get; set; }
+
+        [Required(ErrorMessage = "Field Url Photo is required")]
         public string UrlPhoto { get; set; }
+
+        [Required(ErrorMessage = "Field Email is required")]
+        [EmailAddress(ErrorMessage ="The email format is invalid")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Field Age is required")]
+        [Range(18,100,ErrorMessage = "Age must be between 18 and 100 years old")]
         public int Age { get; set; }
 
     }
